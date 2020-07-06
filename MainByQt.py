31c4144e55442bd5f202f20fd1a2f1bfc74beb00
kaliomar <kaliomar89@yahoo.com>
2020-07-06 15:51:53 +0200

    rename qt module

---
+++ MainByQt.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QFileDialog, QWidget
+import os
+from zipping_module import zipit
+from rename_module import renameit
+
+class Ui_MainWindow(QWidget):
+    def __init__(self):
+        super(Ui_MainWindow, self).__init__()
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(572, 299)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(40, 50, 441, 31))
+        self.textEdit.setObjectName("textEdit")
+        self.button1 = QtWidgets.QPushButton(self.centralwidget)
+        self.button1.setGeometry(QtCore.QRect(480, 50, 61, 31))
+        self.button1.setObjectName("button1")
+        self.button1.clicked.connect(self.open_file)
+        self.progress_bar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progress_bar.setGeometry(QtCore.QRect(40, 170, 501, 23))
+        self.progress_bar.setProperty("value", 24)
+        self.progress_bar.setObjectName("progress_bar")
+        self.button2 = QtWidgets.QPushButton(self.centralwidget)
+        self.button2.setGeometry(QtCore.QRect(450, 100, 91, 27))
+        self.button2.setObjectName("button2")
+        self.button2.clicked.connect(lambda: self.zipping())
+        self.button3 = QtWidgets.QPushButton(self.centralwidget)
+        self.button3.setGeometry(QtCore.QRect(340, 100, 91, 27))
+        self.button3.setObjectName("button3")
+        self.button3.clicked.connect(lambda: self.renaming())
+        self.label1 = QtWidgets.QLabel(self.centralwidget)
+        self.label1.setGeometry(QtCore.QRect(40, 150, 101, 19))
+        self.label1.setObjectName("label1")
+        self.button2.clicked.connect(self.btn_clk)
+        self.button3.clicked.connect(self.btn_clk)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 572, 27))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        self.menuEdit = QtWidgets.QMenu(self.menubar)
+        self.menuEdit.setObjectName("menuEdit")
+        self.menuHelp = QtWidgets.QMenu(self.menubar)
+        self.menuHelp.setObjectName("menuHelp")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionQuit = QtWidgets.QAction(MainWindow)
+        self.actionQuit.setObjectName("actionQuit")
+        self.actionCopy = QtWidgets.QAction(MainWindow)
+        self.actionCopy.setObjectName("actionCopy")
+        self.actionCut = QtWidgets.QAction(MainWindow)
+        self.actionCut.setObjectName("actionCut")
+        self.actionPaste = QtWidgets.QAction(MainWindow)
+        self.actionPaste.setObjectName("actionPaste")
+        self.actionAbout = QtWidgets.QAction(MainWindow)
+        self.actionAbout.setObjectName("actionAbout")
+        self.menuFile.addAction(self.actionQuit)
+        self.menuEdit.addAction(self.actionCopy)
+        self.menuEdit.addAction(self.actionCut)
+        self.menuEdit.addAction(self.actionPaste)
+        self.menuHelp.addAction(self.actionAbout)
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuEdit.menuAction())
+        self.menubar.addAction(self.menuHelp.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Nemorge"))
+        self.button1.setText(_translate("MainWindow", "Open"))
+        self.button2.setText(_translate("MainWindow", "Compress"))
+        self.button3.setText(_translate("MainWindow", "Rename"))
+        self.label1.setText(_translate("MainWindow", "Case"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.menuEdit.setTitle(_translate("MainWindow", "Edit"))
+        self.menuHelp.setTitle(_translate("MainWindow", "Help"))
+        self.actionQuit.setText(_translate("MainWindow", "Quit"))
+        self.actionQuit.setStatusTip(_translate("MainWindow", "Quit the program"))
+        self.actionQuit.setShortcut(_translate("MainWindow", "Alt+Q"))
+        self.actionCopy.setText(_translate("MainWindow", "Copy"))
+        self.actionCopy.setStatusTip(_translate("MainWindow", "Copy path"))
+        self.actionCopy.setShortcut(_translate("MainWindow", "Ctrl+C"))
+        self.actionCut.setText(_translate("MainWindow", "Cut"))
+        self.actionCut.setStatusTip(_translate("MainWindow", "Cut path"))
+        self.actionCut.setShortcut(_translate("MainWindow", "Ctrl+X"))
+        self.actionPaste.setText(_translate("MainWindow", "Paste"))
+        self.actionPaste.setStatusTip(_translate("MainWindow", "Paste path"))
+        self.actionPaste.setShortcut(_translate("MainWindow", "Ctrl+V"))
+        self.actionAbout.setText(_translate("MainWindow", "About"))
+        self.actionAbout.setStatusTip(_translate("MainWindow", "More info"))
+
+    def open_file(self):
+        path = QFileDialog.getExistingDirectory(self,'select a directory','')
+        if path:
+            self.textEdit.setText(path)
+
+    def btn_clk(self):
+        sender = self.sender()
+        if sender.text() == "Compress":
+            self.label1.setText("Compressing...")
+        elif sender.text() == "Rename":
+            self.label1.setText("Renaming...")
+
+    def zipping(self):
+        work_path = self.textEdit.toPlainText()
+        os.chdir(work_path)
+        zipit()
+
+    def renaming(self):
+        work_path = self.textEdit.toPlainText()
+        os.chdir(work_path)
+        renameit()
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
